c9b69f7163c404c3d970377223c053f6b909e471
Shalom Volchok <shalomvolchok@gmail.com>
2019-04-29 15:41:26 +0300

    add check for variation mousedown dataset

--- src/events/variationMousedown.ts
+++ src/events/variationMousedown.ts
@@ -12,40 +12,40 @@ export type VariationMousedown = {
   exposureId: string;
 };
 
-if (!Element.prototype.matches) {
-  Element.prototype.matches = Element.prototype.webkitMatchesSelector;
-}
+// if (!Element.prototype.matches) {
+//   Element.prototype.matches = Element.prototype.webkitMatchesSelector;
+// }
 
-if (!Element.prototype.closest) {
-  Element.prototype.closest = function(s) {
-    var el = this;
+// if (!Element.prototype.closest) {
+//   Element.prototype.closest = function(s) {
+//     var el = this;
 
-    do {
-      if (el.matches(s)) return el;
-      el = el.parentElement || el.parentNode;
-    } while (el !== null && el.nodeType === 1);
-    return null;
-  };
-}
+//     do {
+//       if (el.matches(s)) return el;
+//       el = el.parentElement || el.parentNode;
+//     } while (el !== null && el.nodeType === 1);
+//     return null;
+//   };
+// }
 
 export const variationMousedown$ = fromEvent<MouseEvent>(
   window,
   "mousedown"
 ).pipe(
   map(event => {
-    // if this mousedown event is on a tracked variation then all
-    // these properties should be set on the target dataset
     if (event.target instanceof HTMLElement) {
       const dataset = observedElementMetadata.get(event.target);
 
-      return {
-        type: "VARIATION_MOUSEDOWN",
-        elapsedTime: now(),
-        releaseId: dataset.releaseId,
-        featureId: dataset.featureId,
-        variationId: dataset.variationId,
-        exposureId: dataset.exposureId
-      };
+      if (dataset) {
+        return {
+          type: "VARIATION_MOUSEDOWN",
+          elapsedTime: now(),
+          releaseId: dataset.releaseId,
+          featureId: dataset.featureId,
+          variationId: dataset.variationId,
+          exposureId: dataset.exposureId
+        };
+      }
     }
   }),
   filter(event => event !== undefined)
